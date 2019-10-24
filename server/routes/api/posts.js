b4893f4d0720ba647d7ec9f4827e1824b03b3188
YashanThilakasiriMMYS <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2019-10-24 08:01:21 +0530

    initial backend created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//get
+router.get("/", async (req,res) => {
+    const posts = await loadPostCollection();
+    res.send(await posts.find({}).toArray());
+});
+
+//add
+router.post('/', async (req, res) => {
+    const posts = await loadPostCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+});
+
+//delete
+router.delete('/:id', async (req,res) => {
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id) });
+    res.status(200).send();
+});
+
+
+async function loadPostCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://yashan:123@cluster0-owwih.gcp.mongodb.net/test?retryWrites=true&w=majority', {
+        useNewUrlParser: true
+    });
+    return client.db('vue_express').collection('posts');
+}
+
+module.exports = router;
